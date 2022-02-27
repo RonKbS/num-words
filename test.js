0d242e91c098389cdfcb82cd155e8504b1065e37
RonKbS <stephen.bamwite@gmail.com>
2022-02-27 17:56:57 +0300

    add tests for value 2

--- test.js
+++ test.js
@@ -22,6 +22,16 @@ test('Should convert numbers to words correctly', (t) => {
   assertNum(123456789, 'one hundred twenty three million, four hundred fifty six thousand, seven hundred eighty nine')
   assertNum(1300099010900, 'one trillion, three hundred billion, ninety nine million, ten thousand, nine hundred')
 
+  assertNum(2, 'two')
+  assertNum(23, 'twenty three')
+  assertNum(234, 'two hundred thirty four')
+  assertNum(2345, 'two thousand, three hundred forty five')
+  assertNum(23456, 'twenty three thousand, four hundred fifty six')
+  assertNum(234567, 'two hundred thirty four thousand, five hundred sixty seven')
+  assertNum(2345678, 'two million, three hundred forty five thousand, six hundred seventy eight')
+  assertNum(23456789, 'twenty three million, four hundred fifty six thousand, seven hundred eighty nine')
+  assertNum(234567890, 'two hundred thirty four million, five hundred sixty seven thousand, eight hundred ninety')
+
   t.throws(() => numWords(1e15))
 
   t.end()
